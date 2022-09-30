a01911b51490ab5a6ab36679303f13765c6f06f5
khanh.nguyentranbao <khanh.nguyentranbao@VNNOT01365>
2022-09-30 10:13:09 +0700

    Initial commit

---
+++ AspNetIdentityDemo.Api/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using AspNetIdentityDemo.Api.Services;
+using AspNetIdentityDemo.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace AspNetIdentityDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IUserService _userService;
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // "email": "user@example.com",
+        // "password": "Bk.123"
+
+        // api/auth/register
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterAsync([FromBody]RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.RegisterUserAsync(model);
+                if (result.IsSuccess)
+                {
+                    return Ok(result); // Status code: 200
+                }
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are not valid"); // Status code: 400
+
+        }
+        // api/auth/login
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginAsync([FromBody]LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.LoginUserAsync(model);
+
+                if (result.IsSuccess)
+                {
+                    return Ok(result); // Status code: 200
+                }
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are not valid"); // Status code: 400
+        }
+    }
+}
